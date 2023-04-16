e0b995fbb89194dc39e092589d5b87cdb10b44a4
Badr GHAZAOUI <ghazaoui.badr.tech@gmail.com>
2023-04-16 07:37:40 +0100

    Initial commit

---
+++ src/test/java/com/ensias/harmoniAPI/HarmoniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensias.harmoniAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
