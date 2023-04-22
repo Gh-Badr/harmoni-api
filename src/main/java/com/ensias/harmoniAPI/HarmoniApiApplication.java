e799d55d39ddcba6dacdcb1505cc97a6cd14588f
Badr GHAZAOUI <ghazaoui.badr.tech@gmail.com>
2023-04-22 05:23:42 +0100

    access mongodb credentials through env variables declared inside docker image

--- src/main/java/com/ensias/harmoniAPI/HarmoniApiApplication.java
+++ src/main/java/com/ensias/harmoniAPI/HarmoniApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:mongodb.properties")  //the access mongodb credentials in hidden file mongodb.properties 
+//@PropertySource("classpath:mongodb.properties")  //to access mongodb credentials in hidden file mongodb.properties 
 public class HarmoniApiApplication {
 
 	public static void main(String[] args) {
